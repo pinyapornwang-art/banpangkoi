438a30a8bad055cf6238bd815399a40ae33c6578
pinyapornwang-art <pinyaporn.wang@gmail.com>
2026-09-02 11:44:38 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbxn0AUHsWE3rqY70rcUcsPNvb9p_KQYSbrg4a21c_TrhZuJgEtn2C1gtFuwn910aP2obQ/exec';
